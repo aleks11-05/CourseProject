96807789434a97b7cff43172ed0737ca9b86114e
Arina <arina1105@MacBook-Pro-Arina.local>
2025-06-27 01:52:50 +0300

    Update DBConnection.java

--- src/main/java/org/example/university2/DBConnection.java
+++ src/main/java/org/example/university2/DBConnection.java
@@ -1,5 +1,37 @@
 package org.example.university2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+    public static DBConnection myInstance;
+    private Connection conn;
+
+
+    private final String URL = "jdbc:mysql://localhost:3306/CourseProject3";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+
+
+    DBConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Успешное подключение к базе данных.");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при подключении к базе: " + e.getMessage());
+        }
+    }
+
+    public static DBConnection getInstance() {
+        if (myInstance == null) {
+            myInstance = new DBConnection();
+        }
+        return myInstance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
