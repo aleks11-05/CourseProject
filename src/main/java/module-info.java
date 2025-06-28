be147352bee7d4f3cf63a4f4a985983b090017a6
Arina <arina1105@MacBook-Pro-Arina.local>
2025-06-28 11:08:19 +0300

    updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,15 @@ module org.example.university2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
+    requires java.desktop;
 
+    exports org.example.university2.View;
 
     opens org.example.university2 to javafx.fxml;
     exports org.example.university2;
+    exports org.example.university2.Models;
+    opens org.example.university2.Models to javafx.fxml;
+    exports org.example.university2.Containers;
+    opens org.example.university2.Containers to javafx.fxml;
 }
